40555dcf87e37e591a5efed2f25067b963b44f03
AlexH <>
2024-02-06 17:20:49 -0800

    Django backend, and django backend app to fetch market data

--- g3p2_atb/marketDataFetcher/backend/bars.py
+++ g3p2_atb/marketDataFetcher/backend/bars.py
@@ -1,27 +1,27 @@
-import config, requests
+import config, requests, json
 from datetime import datetime
 import pandas as pd
 
 holdings = pd.read_csv('C:\\Users\\Alex\\Desktop\\FinTech-CourseWork\\Projects\\Project_2\\g3p2\\g3p2_atb\\marketDataFetcher\\backend\\data\\qqq.csv')
 for holding in holdings:
-    print(holding)
+  print(holding)
 
 symbols = holdings['Holding Ticker']
 symbols = ",".join(symbols)
 
 day_bars_url = '{}/day?symbols={}&limit=1000'.format(config.BARS_URL, symbols)
 
-request = requests.get(config.BARS_URL, headers=config.HEADERS)
+request = requests.get(day_bars_url, headers=config.HEADERS)
 data = request.json()
 
 for symbol in data:
-    filename = 'C:\\Users\\Alex\\Desktop\\FinTech-CourseWork\\Projects\\Project_2\\g3p2\\g3p2_atb\\marketDataFetcher\\backend\\data\\ohlc\\message.txt'.format(symbol)
-    f = open(filename, 'w+')
-    f.write('Date,Open,High,Low,Close,Volume,OpenInterest\n')
+  filename = 'C:\\Users\\Alex\\Desktop\\FinTech-CourseWork\\Projects\\Project_2\\g3p2\\g3p2_atb\\marketDataFetcher\\backend\\data\\ohlc\\message.txt'.format(symbol)
+  f = open(filename, 'w+')
+  f.write('Date,Open,High,Low,Close,Volume,OpenInterest\n')
 
-    for bar in data[symbol]:
-        t = datetime.fromtimestamp(bar['t'])
-        day = t.strftime('%Y-%m-%d')
-        
-        line = '{},{},{},{},{},{},{}\n'.format(day, bar['o'], bar['h'], bar['l'], bar['c'], bar['v'], 0.00)
-        f.write(line)
+  for bar in data[symbol]:
+    t = datetime.fromtimestamp(bar['t'])
+    day = t.strftime('%Y-%m-%d')
+  
+    line = '{},{},{},{},{},{},{}\n'.format(day, bar['o'], bar['h'], bar['l'], bar['c'], bar['v'], 0.00)
+    f.write(line)
